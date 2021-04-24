8e7d292730e978e9cef0da187e89fcf7f8d22afb
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-04-24 16:01:47 +0200

    toggle submenu

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const burger = document.querySelector(".burger");
 const closeX = document.querySelector(".close-x");
 const leistungen = document.querySelector(".leistungen");
 
-
 function visibleSubMenu() {
   subMenu.classList.remove("invisible");
   subMenu.classList.add("visible");
@@ -12,7 +11,12 @@ function inVisibleSubMenu() {
   subMenu.classList.add("invisible");
   subMenu.classList.remove("visible");
 }
+function toggleSubmenu() {
+  subMenu.classList.contains("invisible") === true
+    ? visibleSubMenu()
+    : inVisibleSubMenu();
+}
 
 burger.addEventListener("click", visibleSubMenu);
 closeX.addEventListener("click", inVisibleSubMenu);
-leistungen.addEventListener("click", visibleSubMenu);
+leistungen.addEventListener("click", toggleSubmenu);
