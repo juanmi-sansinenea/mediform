6da0da79ac6ccf1dfb5c6ed063f3da8a15cc2986
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-06-20 23:29:14 +0200

    two swipers on the praxis page working smooth like butter

--- master.js
+++ master.js
@@ -353,26 +353,26 @@ customElements.define("testimonial-slider", TestimonialSlider);
 /////////////   SWIPER  ////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////
 
-const swiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+// const swiper = new Swiper(".swiper-container", {
+//   // Optional parameters
+//   direction: "horizontal",
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: "true",
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     type: "bullets",
+//     clickable: "true",
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
 
-  // And if we need scrollbar
-  // scrollbar: {
-  //   el: '.swiper-scrollbar',
-  // },
-});
+//   // And if we need scrollbar
+//   // scrollbar: {
+//   //   el: '.swiper-scrollbar',
+//   // },
+// });
