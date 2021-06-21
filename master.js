77fe89aa371b1f5083eb52cb7afb205fc66a1b21
Juan Miguel Sansinenea <juanmi.sansinenea@gmail.com>
2021-06-21 23:01:36 +0200

    testimonial slider mobile initial commit

--- master.js
+++ master.js
@@ -306,48 +306,7 @@ class TeaserSmall extends HTMLElement {
 }
 customElements.define("teaser-small", TeaserSmall);
 
-//////////////////////////////////////////////////////////////////////////////////////
-/////////////   TESTIMONNIAL SLIDER  /////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////
 
-class TestimonialSlider extends HTMLElement {
-  constructor() {
-    super();
-    this.slides = this.querySelectorAll(".slide");
-    this.prev = this.querySelector(".prev");
-    this.next = this.querySelector(".next");
-
-    this.showActiveSlide = (to) => {
-      for (let i = 0; i < this.slides.length; i++) {
-        this.slides[i].style.display = "none";
-      }
-      this.slides[this.activeSlide].style.display = "";
-      this.slides[
-        this.activeSlide
-      ].style.animation = `slide-in-${to} ease 0.75s`;
-      this.slides[this.activeSlide].addEventListener("animationend", () => {
-        console.log("animation ended");
-      });
-    };
-    this.prev.addEventListener("click", () => {
-      this.activeSlide > 0
-        ? this.activeSlide--
-        : (this.activeSlide = this.slides.length - 1);
-      this.showActiveSlide("prev");
-    });
-    this.next.addEventListener("click", () => {
-      this.activeSlide < this.slides.length - 1
-        ? this.activeSlide++
-        : (this.activeSlide = 0);
-      this.showActiveSlide("next");
-    });
-  }
-  connectedCallback() {
-    this.activeSlide = 0;
-    this.showActiveSlide();
-  }
-}
-customElements.define("testimonial-slider", TestimonialSlider);
 
 //////////////////////////////////////////////////////////////////////////////////////
 /////////////   SWIPER  ////////////////////////////////////////////
